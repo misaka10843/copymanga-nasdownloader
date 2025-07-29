24e6cb10b4be887868e6b7890ee8b0ebe954b850
misaka10843 <misaka10843@outlook.jp>
2025-07-29 10:54:03 +0800

    开始准备添加新平台

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import logging
 
-import updater
+from plugins.copymanga import updater
 from plugins.copymanga.main import copymanga_downloader
-from utils import request
 from utils.log import configure_logging
 
 configure_logging()
@@ -10,30 +9,7 @@ configure_logging()
 log = logging.getLogger(__name__)
 
 def main():
-    download_list = updater.process_updates()
-
-    if not download_list:
-        log.info("当前没有需要更新的内容")
-        return
-
-    log.info(f"检测到{len(download_list)}个漫画有更新内容")
-    for comic in download_list:
-        debug = (
-                f"漫画名称: {comic['name']}\n"
-                f"路径标识: {comic['path_word']}\n"
-                f"当前章节: {comic['current_chapter']}\n"
-                f"待更新数: {len(comic['uuids'])}\n"
-                f"UUID列表:\n" + "\n".join([f"  - {uuid}" for uuid in comic['uuids']])
-        )
-        info = (
-            f"漫画名称: {comic['name']}\n"
-            f"当前章节: {comic['current_chapter'] or '无'}\n"
-            f"待更新数: {len(comic['uuids'])}"
-        )
-        log.debug(debug)
-        log.info(info)
-        copymanga_downloader(comic)
-    log.info(f"需要更新的所有漫画下载已完成")
+    copymanga_downloader()
 
 
 if __name__ == '__main__':
--- plugins/copymanga/main.py
+++ plugins/copymanga/main.py
@@ -2,12 +2,14 @@ import logging
 import os
 import time
 
-import updater
 from downloader import downloader, postprocess
+from plugins.copymanga import updater
 from utils import config, request
 from utils.rename import rename_series
 
 log = logging.getLogger(__name__)
+
+
 def get_chapter(path_word: str, uuid: str):
     data = request.get(f"/api/v3/comic/{path_word}/chapter2/{uuid}")
     try:
@@ -15,7 +17,8 @@ def get_chapter(path_word: str, uuid: str):
     except Exception as e:
         log.error(f"漫画章节内容解析失败：{e}")
 
-def copymanga_downloader(comic):
+
+def main(comic):
     for uuid in comic['uuids']:
         chapter = get_chapter(comic['path_word'], uuid)
         log.info(f"已获取到{comic['name']} {chapter['name']}的内容，开始安排下载")
@@ -44,3 +47,30 @@ def copymanga_downloader(comic):
         log.info(f"{comic['name']} {chapter['name']} cbz打包完成，等待三秒继续")
         time.sleep(3)
     log.info(f"{comic['name']} 需要更新的下载已完成")
+
+
+def copymanga_downloader():
+    download_list = updater.process_updates()
+
+    if not download_list:
+        log.info("当前没有需要更新的内容")
+        return
+
+    log.info(f"检测到{len(download_list)}个漫画有更新内容")
+    for comic in download_list:
+        debug = (
+                f"漫画名称: {comic['name']}\n"
+                f"路径标识: {comic['path_word']}\n"
+                f"当前章节: {comic['current_chapter']}\n"
+                f"待更新数: {len(comic['uuids'])}\n"
+                f"UUID列表:\n" + "\n".join([f"  - {uuid}" for uuid in comic['uuids']])
+        )
+        info = (
+            f"漫画名称: {comic['name']}\n"
+            f"当前章节: {comic['current_chapter'] or '无'}\n"
+            f"待更新数: {len(comic['uuids'])}"
+        )
+        log.debug(debug)
+        log.info(info)
+        main(comic)
+    log.info(f"需要更新的所有漫画下载已完成")
